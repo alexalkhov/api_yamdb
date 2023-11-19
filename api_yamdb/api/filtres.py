4ab631c4b63dfbea8cd219f27f6a7556e1cd90e4
Artem Pokhilko <pohilko.tema@yandex.ru>
2023-11-19 15:35:06 +0300

    finished the code second developer

--- api_yamdb/api/filtres.py
+++ api_yamdb/api/filtres.py
@@ -1,25 +1,13 @@
-# from django_filters import rest_framework as filters
-# from reviews.models import Title
+from django_filters.rest_framework import CharFilter, FilterSet, NumberFilter
+from reviews.models import Title
 
 
-# class TitleFilter(filters.FilterSet):
-#     category = filters.CharFilter(
-#         field_name='category__slug',
-#         lookup_expr='iexact'
-#     )
-#     genre = filters.CharFilter(
-#         field_name='genre__slug',
-#         lookup_expr='iexact'
-#     )
-#     name = filters.CharFilter(
-#         field_name='name',
-#         lookup_expr='iexact'
-#     )
-#     year = filters.NumberFilter(
-#         field_name='year',
-#         lookup_expr='iexact'
-#     )
+class TitleFilter(FilterSet):
+    category = CharFilter(field_name='category__slug')
+    genre = CharFilter(field_name='genre__slug')
+    name = CharFilter(field_name='name')
+    year = NumberFilter(field_name='year')
 
-#     class Meta:
-#         model = Title
-#         fields = '__all__'
+    class Meta:
+        model = Title
+        fields = ('name', 'category', 'genre', 'year')
